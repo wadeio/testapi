f4cb7b2535edbb783f0ebc247bd73e6a4ecc5ae2
User <user@Userde-MacBook-Air.local>
2019-05-20 12:55:08 +0800

    add 2

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const app=express();
 const port=3000;
 
 app.get('/',(req,res)=>{
-    //res.write("hellow");
-    res.sendFile('index.html');
-    res.end();
-})
+    //res.write(__dirname);
+    //res.render('index');
+    res.sendFile(__dirname+'/index.html');
+});
 
-app.listen(3000,function(){
-    console.log('server start on http://localhost:3000');
+app.listen(80,function(){
+    console.log('server start on http://localhost');
 })
